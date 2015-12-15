96fb89b5960e501320e355e15f0004eb2654516b
João Rodrigues Jr <mail@joaorodriguesjr.com>
2015-12-15 21:56:47 -0200

    adds ECMAScript 2015 compiling

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,21 @@
-var gulp = require('gulp');
+var gulp  = require('gulp');
+var babel = require('gulp-babel');
+var maps  = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
 
-gulp.task('default', []);
+gulp.task('compile:js', function () {
+    return gulp.src('./src/*.es6')
+        .pipe(maps.init())
+        .pipe(babel())
+        .pipe(maps.write())
+        .pipe(gulp.dest('./src'));
+});
+
+gulp.task('watch', function () {
+    watch('./src/*.es6', function () {
+        gulp.start('compile:js');
+    });
+});
+
+gulp.task('compile', ['compile:js']);
+gulp.task('default', ['compile', 'watch']);
