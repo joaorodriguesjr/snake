976333fad97a373310647b60883bc1a2c50eb3c2
João Rodrigues Jr <mail@joaorodriguesjr.com>
2015-12-15 23:12:23 -0200

    adds initialization

--- spec/SnakeSpec.js
+++ spec/SnakeSpec.js
@@ -4,4 +4,35 @@ describe('Snake', function () {
     beforeEach(function () {
         snake = new Snake();
     });
+
+    it('is in row 1', function () {
+        expect(snake.head.row).toBe(1);
+    });
+
+    it('is in col 1', function () {
+        expect(snake.head.col).toBe(1);
+    });
+
+    it('is in row 2', function () {
+        snake.init({row: 2, col: 1});
+        expect(snake.head.row).toBe(2);
+    });
+
+    it('is in col 2', function () {
+        snake.init({row: 1, col: 2});
+        expect(snake.head.col).toBe(2);
+    });
+
+    it('has parts', function () {
+        expect(snake.parts.length).toBe(1);
+    });
+
+    it('is going right', function () {
+        expect(snake.direction).toBe(Direction.RIGHT);
+    });
+
+    it('is going left', function () {
+        snake.init(undefined, Direction.LEFT);
+        expect(snake.direction).toBe(Direction.LEFT);
+    });
 });
--- src/Snake.es6
+++ src/Snake.es6
@@ -1,3 +1,10 @@
 class Snake {
-    constructor() {}
+    constructor() {
+        this.init();
+    }
+
+    init(position, direction) {
+        this.parts = [this.head = position || {row: 1, col: 1}];
+        this.direction = direction || Direction.RIGHT;
+    }
 }
