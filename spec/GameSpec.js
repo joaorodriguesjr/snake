a6fbd9c2355279a26488a938f495546e26035800
João Rodrigues Jr <mail@joaorodriguesjr.com>
2015-12-16 00:32:01 -0200

    adds initialization

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,10 +1,13 @@
 describe('Game', function () {
     var game;
-    var grid  = {};
-    var snake = {};
+    var grid  = {init: function () {}};
+    var snake = {init: function () {}};
 
     beforeEach(function () {
-        game = new Game(grid, snake);
+        spyOn(grid , 'init');
+        spyOn(snake, 'init');
+
+        game  = new Game(grid, snake);
     });
 
     it('holds a Grid object', function () {
@@ -13,5 +16,25 @@ describe('Game', function () {
 
     it('holds a Snake object', function () {
         expect(game.snake).toBe(snake);
+    });
+
+    it('initializes grid', function () {
+        expect(grid.init).toHaveBeenCalled();
+    });
+
+    it('initializes snake', function () {
+        expect(snake.init).toHaveBeenCalled();
+    });
+
+    it('has 10 levels', function () {
+        expect(game.levels.length).toBe(9); // 9 + 1 at "game.level"
+    });
+
+    it('is at level 1', function () {
+        expect(game.level.ID).toBe(1);
+    });
+
+    it('has score 0', function () {
+        expect(game.score).toBe(0);
     });
 });
--- src/Game.es6
+++ src/Game.es6
@@ -2,5 +2,31 @@ class Game {
     constructor(grid, snake) {
         this.grid  = grid;
         this.snake = snake;
+
+        this.init();
+    }
+
+    init() {
+        this.initLevels();
+
+        this.score = 0;
+        this.level = this.levels.shift();
+        this.grid.init();
+        this.snake.init();
+    }
+
+    initLevels() {
+        this.levels = [
+            {ID: 1 , speed: 10, score: 50 , up: 200  },
+            {ID: 2 , speed: 9 , score: 75 , up: 500  },
+            {ID: 3 , speed: 8 , score: 100, up: 1500 },
+            {ID: 4 , speed: 7 , score: 125, up: 3000 },
+            {ID: 5 , speed: 6 , score: 150, up: 5000 },
+            {ID: 6 , speed: 5 , score: 175, up: 7000 },
+            {ID: 7 , speed: 4 , score: 200, up: 9000 },
+            {ID: 8 , speed: 3 , score: 250, up: 10000},
+            {ID: 9 , speed: 2 , score: 300, up: 11000},
+            {ID: 10, speed: 1 , score: 500, up: 12000}
+        ];
     }
 }
