c9ea846e41bf4cb3f366a93c4066c5822a43a5ab
João Rodrigues Jr <mail@joaorodriguesjr.com>
2015-12-16 18:09:09 -0200

    adds a running environment

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ gulp.task('compile:js', function () {
         .pipe(maps.init())
         .pipe(babel())
         .pipe(maps.write())
-        .pipe(gulp.dest('./src'));
+        .pipe(gulp.dest('./src'))
+        .pipe(gulp.dest('./run'));
 });
 
 gulp.task('watch', function () {
