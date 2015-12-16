b382e4cabbe70bfb3e6e8afe034988924af96931
João Rodrigues Jr <mail@joaorodriguesjr.com>
2015-12-15 22:50:09 -0200

    adds the Direction class

---
+++ spec/DirectionSpec.js
@@ -0,0 +1,17 @@
+describe('Direction', function () {
+    it('has a static property LEFT', function () {
+        expect(Direction.LEFT).toBe(1);
+    });
+
+    it('has a static property UP', function () {
+        expect(Direction.UP).toBe(2);
+    });
+
+    it('has a static property RIGHT', function () {
+        expect(Direction.RIGHT).toBe(3);
+    });
+
+    it('has a static property DOWN', function () {
+        expect(Direction.DOWN).toBe(4);
+    });
+});
---
+++ src/Direction.es6
@@ -0,0 +1,6 @@
+class Direction {}
+
+Direction.LEFT  = 1;
+Direction.UP    = 2;
+Direction.RIGHT = 3;
+Direction.DOWN  = 4;
