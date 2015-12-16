b179375c7c36bc915e00121457bfa83e2ebf0b9b
João Rodrigues Jr <mail@joaorodriguesjr.com>
2015-12-15 22:20:05 -0200

    adds initialization

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -1,7 +1,23 @@
 describe('Grid', function () {
     var grid;
+    var dimensions = {rows: 15, cols: 20};
 
     beforeEach(function () {
-        grid = new Grid();
+        grid = new Grid(dimensions);
+    });
+
+    it('has 15 rows', function () {
+        expect(grid.cells.length).toBe(15);
+    });
+
+    it('has 20 columns', function () {
+        expect(grid.cells[0].length).toBe(20);
+    });
+
+    it('has all cells initialized with 0', function () {
+        for (var row = 0; row < dimensions.rows; row += 1) {
+        for (var col = 0; col < dimensions.cols; col += 1) {
+            expect(grid .cells[row][col]).toBe(0);
+        }}
     });
 });
--- src/Grid.es6
+++ src/Grid.es6
@@ -1,4 +1,25 @@
 class Grid {
-    constructor() {
+    constructor(dimensions) {
+        this.dimensions = dimensions;
+        this.init();
+    }
+
+    init() {
+        this.cells = [];
+        this.initRows();
+        this.initCols();
+    }
+
+    initRows() {
+        for (let row = 0; row < this.dimensions.rows; row += 1) {
+            this.cells[row] = [];
+        }
+    }
+
+    initCols() {
+        for (let row = 0; row < this.dimensions.rows; row += 1) {
+        for (let col = 0; col < this.dimensions.cols; col += 1) {
+            this.cells[row][col] = 0;
+        }}
     }
 }
