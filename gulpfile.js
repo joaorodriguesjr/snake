8b125d02b602d489ec009f0f4fb3e6d1de4b67f7
João Rodrigues Jr <mail@joaorodriguesjr.com>
2015-12-17 10:49:25 -0200

    uses the ECMAScript 2015 features

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
-var gulp  = require('gulp');
-var babel = require('gulp-babel');
-var maps  = require('gulp-sourcemaps');
-var watch = require('gulp-watch');
+(function() {
+    'use strict';
 
-gulp.task('compile:js', function () {
-    return gulp.src('./src/*.es6')
-        .pipe(maps.init())
-        .pipe(babel())
-        .pipe(maps.write())
-        .pipe(gulp.dest('./src'));
-});
+    let gulp  = require('gulp');
+    let babel = require('gulp-babel');
+    let maps  = require('gulp-sourcemaps');
+    let watch = require('gulp-watch');
 
-gulp.task('watch', function () {
-    watch('./src/*.es6', function () {
-        gulp.start('compile:js');
+    gulp.task('compile:js', function () {
+        return gulp.src('./src/*.es6')
+            .pipe(maps.init())
+            .pipe(babel())
+            .pipe(maps.write())
+            .pipe(gulp.dest('./src'));
     });
-});
 
-gulp.task('compile', ['compile:js']);
-gulp.task('default', ['compile', 'watch']);
+    gulp.task('watch', function () {
+        watch('./src/*.es6', () => gulp.start('compile:js'));
+    });
+
+    gulp.task('compile', ['compile:js']);
+    gulp.task('default', ['compile', 'watch']);
+
+}());
