5aeee92b0ac201d8bcca23b21cecfac1698e9f03
João Rodrigues Jr <mail@joaorodriguesjr.com>
2015-12-17 11:13:14 -0200

    adds build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 (function() {
     'use strict';
 
-    let gulp  = require('gulp');
-    let babel = require('gulp-babel');
-    let maps  = require('gulp-sourcemaps');
-    let watch = require('gulp-watch');
+    let gulp    = require('gulp');
+    let babel   = require('gulp-babel');
+    let maps    = require('gulp-sourcemaps');
+    let concat  = require('gulp-concat');
+    let uglify  = require('gulp-uglify');
+    let cssmin  = require('gulp-cssmin');
+    let htmlmin = require('gulp-htmlmin');
+    let replace = require('gulp-html-replace');
+    let watch   = require('gulp-watch');
 
     gulp.task('compile:js', function () {
         return gulp.src('./src/*.es6')
@@ -14,11 +19,43 @@
             .pipe(gulp.dest('./src'));
     });
 
+    gulp.task('build:js', function () {
+        let files = [
+            './src/Cell.js',
+            './src/Grid.js',
+            './src/Direction.js',
+            './src/Snake.js',
+            './src/Game.js',
+            './src/Input.js',
+            './src/Renderer.js',
+            './src/main.js',
+        ];
+
+        return gulp.src(files)
+            .pipe(concat('index.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('./build'));
+    });
+
+    gulp.task('build:css', function () {
+        return gulp.src('./run/index.css')
+            .pipe(cssmin())
+            .pipe(gulp.dest('./build'));
+    });
+
+    gulp.task('build:html', function () {
+        return gulp.src('./run/index.html')
+            .pipe(replace({js: 'index.js', css: 'index.css'}))
+            .pipe(htmlmin({collapseWhitespace: true}))
+            .pipe(gulp.dest('./build'));
+    });
+
     gulp.task('watch', function () {
         watch('./src/*.es6', () => gulp.start('compile:js'));
     });
 
     gulp.task('compile', ['compile:js']);
+    gulp.task('build'  , ['compile', 'build:js', 'build:css', 'build:html']);
     gulp.task('default', ['compile', 'watch']);
 
 }());
